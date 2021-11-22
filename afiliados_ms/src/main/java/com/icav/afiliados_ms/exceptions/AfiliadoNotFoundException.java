e90e0c4bedba0e8bfc6f091fbd53dcff8b0a2cb3
danieldagc96 <daaguzmanco@unal.edu.co>
2021-11-22 15:44:31 -0500

    exceptions

---
+++ afiliados_ms/src/main/java/com/icav/afiliados_ms/exceptions/AfiliadoNotFoundException.java
@@ -0,0 +1,7 @@
+package com.icav.afiliados_ms.exceptions;
+
+public class AfiliadoNotFoundException extends RuntimeException {
+    public AfiliadoNotFoundException(String message) {
+        super(message);
+    }
+}

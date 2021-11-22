2117b8b3299237a1921c1d25c1087801924c31e3
danieldagc96 <daaguzmanco@unal.edu.co>
2021-11-22 12:55:03 -0500

    configuración inicial Spring Boot

---
+++ afiliados_ms/src/main/java/com/icav/afiliados_ms/AfiliadosMsApplication.java
@@ -0,0 +1,13 @@
+package com.icav.afiliados_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfiliadosMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfiliadosMsApplication.class, args);
+	}
+
+}
